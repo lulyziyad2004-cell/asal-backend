2e7c09a7e973bd6c6fe3d7e3f0abfbc78703c34b
lulyziyad2004-cell <lulyziyad2004@gmail.com>
2026-08-18 01:17:41 +0300

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -1,19 +1,25 @@
-﻿<?php
+<?php
 
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://asal-frontend-coral.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => [
+        'Content-Type',
+        'Authorization',
+        'X-Requested-With',
+        'Accept',
+        'Origin',
+    ],
 
     'exposed_headers' => [],
 
-    'max_age' => 0,
+    'max_age' => 86400,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 ];
