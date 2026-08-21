f20921311c94636d1d34ae55205c9301ef5e96eb
lulyziyad2004-cell <lulyziyad2004@gmail.com>
2026-08-22 02:31:31 +0300

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,17 +1,20 @@
 <?php
+
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
-return Application::configure(basePath: dirname(_ _DIR_ _))
+
+return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web:_ _DIR_ _ . '/../routes/web.php',
-        api: _ _DIR_ _ . '/../routes/api.php',
-        commands: _ _DIR_ _ . '/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
+
         $middleware->alias([
             'admin' => \App\Http\Middleware\AdminMiddleware::class,
             'role' => \App\Http\Middleware\RoleMiddleware::class,
